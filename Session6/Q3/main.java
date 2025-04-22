84ad23cff25d608032532d9cf5c08bcf30f0e4e8
Swayam Khatter <swayam.khatter@metacube.com>
2025-04-22 10:58:17 +0530

    .....

--- Session6/Q3/main.java
+++ Session6/Q3/main.java
@@ -1,6 +1,6 @@
 package Session6.Q3;
 
-public class main {
+public class Main {
     public static void main(String[] args) {
         EmployeeListQuickSort obj = new EmployeeListQuickSort();
         obj.add("Swayam", 20000, 21);
